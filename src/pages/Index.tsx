25b7140415f8b08c2c969b5b47af9c3d011df353
limaalves595-alt <limaalves595@gmail.com>
2026-02-21 19:20:51 -0300

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import ServicesSection from "@/components/ServicesSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import AboutSection from "@/components/AboutSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <ServicesSection />
+      <PortfolioSection />
+      <AboutSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
